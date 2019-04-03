b8d0c80e61d4ca314263a9396456cfc3759bfc89
Kyle Carter <elyko.tech@gmail.com>
2019-04-03 15:44:25 -0400

    Person CRUD app implemented

--- src/main/java/io/zipcoder/crudapp/controllers/PersonController.java
+++ src/main/java/io/zipcoder/crudapp/controllers/PersonController.java
@@ -1,45 +1,51 @@
 package io.zipcoder.crudapp.controllers;
 
 import io.zipcoder.crudapp.models.Person;
+import io.zipcoder.crudapp.repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class PersonController {
 
-    private List<Person> people;
+    private final PersonRepository personRepository;
 
-    public PersonController() {
-        this.people = new ArrayList<>();
+    @Autowired
+    public PersonController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
     }
 
-    @PostMapping(value = "/people")
-    ResponseEntity<Person> createPerson(@RequestBody Person p){
-        this.people.add(p.getId(), p);
+    @PostMapping(value = "/people/")
+    public ResponseEntity<Person> createPerson(@RequestBody Person p){
+        personRepository.save(p);
         return new ResponseEntity<>(p, HttpStatus.CREATED);
     }
 
     @GetMapping(value = "/people/{id}")
-    ResponseEntity<Person> getPerson(@PathVariable Integer id){
-        return new ResponseEntity<>(this.people.get(id), HttpStatus.OK);
+    public ResponseEntity<Person> getPerson(@PathVariable Integer id){
+        return new ResponseEntity<>(personRepository.findOne(id), HttpStatus.OK);
     }
 
-    @GetMapping(value = "/people")
-    ResponseEntity<List<Person>> getPersonList(){
-        return new ResponseEntity<>(this.people, HttpStatus.OK);
+    @GetMapping(value = "/people/")
+    public ResponseEntity<List<Person>> getPersonList(){
+        return new ResponseEntity<>((List<Person>) personRepository.findAll(), HttpStatus.OK);
     }
 
     @PutMapping(value = "/people/{id}")
-    ResponseEntity<Person> updatePerson(@PathVariable Integer id, @RequestBody Person p){
-        this.people.add(id, p);
+    public ResponseEntity<Person> updatePerson(@PathVariable Integer id, @RequestBody Person p){
+        Person originalPerson = personRepository.findOne(id);
+        originalPerson.setFirstName(p.getFirstName());
+        originalPerson.setLastName(p.getLastName());
+        personRepository.save(originalPerson);
         return new ResponseEntity<>(p, HttpStatus.OK);
     }
 
     @DeleteMapping(value = "/people/{id}")
-    ResponseEntity<Boolean> deletePerson(@PathVariable Integer id){
-        return new ResponseEntity<>(this.people.remove(id),HttpStatus.OK) ;
+    public ResponseEntity<Boolean> deletePerson(@PathVariable Integer id){
+        personRepository.delete(id);
+        return new ResponseEntity<>(HttpStatus.OK) ;
     }
 }
--- src/main/java/io/zipcoder/crudapp/models/Person.java
+++ src/main/java/io/zipcoder/crudapp/models/Person.java
@@ -7,41 +7,36 @@ import javax.persistence.Id;
 @Entity
 public class Person {
 
-    private String fName;
-    private String lName;
-
     @Id
     @GeneratedValue
     private Integer id;
 
-
-    public Person(String fName, String lName, Integer id) {
-        this.fName = fName;
-        this.lName = lName;
-        this.id = id;
-    }
-
-    public String getfName() {
-        return fName;
-    }
-
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public void setlName(String lName) {
-        this.lName = lName;
-    }
+    private String firstName;
+    private String lastName;
 
     public Integer getId() {
-        return id;
-    }
+    return id;
+}
 
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String fName) {
+        this.firstName = fName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lName) {
+        this.lastName = lName;
+    }
+
+
 }
